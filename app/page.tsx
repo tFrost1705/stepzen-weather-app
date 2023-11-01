91ace4ff99bd57ccd5b61db92e1cab7fe53a6e18
Mike A <michael.alvarez17@outlook.com>
2023-10-31 19:36:23 -0700

    initial repo init

---
+++ app/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+import CityPicker from "@/components/CityPicker";
+import { Card, Divider, Subtitle, Text } from "@tremor/react";
+//mport { Card, Text, Metric, Flex, ProgressBar } from "@tremor/react";
+//LOOK UP
+/**
+ * 1) next.js and 'use client'
+ *
+ */
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center items-center ">
+      <Card className="max-4xl mx-auto bg-white">
+        <Text className="text-6xl font-bold  text-center mb-10">
+          Weather AI
+        </Text>
+        <Subtitle className="text-xl text-center">
+          Powered by OpenAI, Next.js, Tailwind CSS, Tremor 2.0
+        </Subtitle>
+
+        <Divider className="my-10" />
+        <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E]">
+          <CityPicker />
+        </Card>
+      </Card>
+    </div>
+  );
+}
---
+++ components/CityPicker.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CityPicker() {
+  return <div>CityPicker</div>;
+}
+
+export default CityPicker;
